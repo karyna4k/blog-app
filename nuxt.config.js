5e998e08c7b2f9bba53d610b6335b3bac005bed7
karyna.kananovich <karyna4k.dev@gmail.com>
2023-08-31 18:44:54 +0400

    chore: update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,20 @@
+import axios from 'axios'
+
 export default {
+  target: 'static',
+  ssr: false,
+
+  loading: {
+    color: '#9234eb',
+    failedColor: '#FF392B',
+    height: '2px',
+  },
+  loadingIndicator: '~/loading.html',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog-nuxt',
+    title: 'Blog',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ru',
     },
     meta: [
       { charset: 'utf-8' },
@@ -11,11 +22,25 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/css/main.css', '@/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -23,24 +48,43 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: [({ isLegacy }) => isLegacy && 'axios'],
+    extractCSS: true,
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+
+  router: {
+    linkActiveClass: 'active',
+    linkExactActiveClass: 'active-exact',
+  },
+
+  generate: {
+    routes() {
+      return axios
+        .get('https://blog-nuxt-test-default-rtdb.firebaseio.com/posts.json')
+        .then((res) => {
+          // Get id
+          const postsArray = []
+          for (const key in res.data) {
+            postsArray.push({ ...res.data[key], id: key })
+          }
+          // Routes
+          return postsArray.map((post) => {
+            return '/posts/' + post.id, '/create/' + post.id
+          })
+        })
+    },
+  },
 }
