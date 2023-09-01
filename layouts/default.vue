45c1b659ee6348433089316d53270a6419bfe4eb
karyna.kananovich <karyna4k.dev@gmail.com>
2023-09-01 14:56:13 +0400

    feat: update layouts

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <app-header />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    AppHeader: () => import('~/components/AppHeader.vue'),
+  },
+}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <error-content
+      :header="notFound ? 404 : ''"
+      :title="error.statusCode"
+      :details="error.message"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutsError',
+  props: {
+    error: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  head() {
+    const title = 'My blog'
+    const descr = 'My blog with Nuxt.js!'
+    const type = 'site'
+
+    return {
+      title,
+      meta: [
+        { hid: 'og: title', name: 'og: title', content: title },
+        { hid: 'description', name: 'description', content: descr },
+        { hid: 'og:description', name: 'og:description', content: descr },
+        { hid: 'og:type', name: 'og:type', content: type },
+      ],
+    }
+  },
+  computed: {
+    notFound() {
+      return this.error.statusCode === 404
+    },
+  },
+}
+</script>
